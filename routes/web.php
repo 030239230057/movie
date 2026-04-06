0019c43bfa1b039ac485714019eb0d5c074dfe1a
ltphuonganh <030239230007@st.buh.edu.vn>
2026-04-07 00:39:51 +0700

    Hoàn thành câu 4

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController3;
 
 Route::get('/', [App\Http\Controllers\MovieController::class, 'index']);
+Route::get('/movie/create', [MovieController3::class, 'create']);
+
+Route::post('/movie/store', [MovieController3::class, 'store']);
