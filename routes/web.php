1310cc26abc562566fc1a48f6b286d55a4e04cbd
Thu Hoai <030239230057@st.buh.edu.vn>
2026-04-07 00:22:10 +0700

    Câu 3

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,5 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MovieController;
 
-Route::get('/', function () {
-    return view('welcome'); 
-});
-
-Route::get('/admin/movies', [MovieController::class, 'trangQuanLy'])->name('admin.movies');
-
-Route::get('/admin/movies/delete/{id}', [MovieController::class, 'xoaMemPhim'])->name('admin.movies.delete');
+Route::get('/', [App\Http\Controllers\MovieController::class, 'index']);
