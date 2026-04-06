311e8e7237b3359dce173db87402177ca14f8a5c
Thu Hoai <030239230057@st.buh.edu.vn>
2026-04-07 00:37:38 +0700

    Câu 3 đã chỉnh sửa

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
 
-Route::get('/', [App\Http\Controllers\MovieController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome'); 
+});
+
+Route::get('/admin/movies', [MovieController::class, 'trangQuanLy'])->name('admin.movies');
+
+// 3. Chức năng Xóa phim
+Route::get('/admin/movies/delete/{id}', [MovieController::class, 'xoaMemPhim'])->name('admin.movies.delete');
